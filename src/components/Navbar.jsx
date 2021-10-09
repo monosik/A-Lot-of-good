4ddc42adf3cdfa30ecab8ec17d241a00e1e5b5e6
monosik <tot74800@gmail.com>
2021-10-09 15:09:24 +0700

    Co-authored-by: K656l <K656l@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,99 +1,101 @@
-import { Badge } from '@material-ui/core';
+import { Badge } from "@material-ui/core";
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import { Search } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
-import { mobile, iphone10 } from '../responsive';
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
 import './Navbar.css'
+import { Link } from 'react-router-dom'
+import Sticky from "react-sticky-el";
 
-const Container = styled.div `
-    height: 70px;
+const Container = styled.div`
+    height: 60px;
     position: sticky;
-    ${mobile({ height: "20px", margin: "30px none"})}
-    ${iphone10({ height: "20px", margin: "30px none"})}
-`
+    position: -webkit-sticky;
+    z-index: 2;
+    ${mobile({ height: "50px" })}
+`;
 
-const Wrapper = styled.div `
-    padding: 5px 20px;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    position: sticky;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    position: sticky;
-    ${mobile({ padding: "10px 0px"})}
-`
-const Left = styled.div `
+    ${mobile({ padding: "10px 0px" })}
+`;
+
+const Left = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
-    
-`
+`;
 
-const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;
-    ${mobile({ display: "none"})}
-`
-
-const SearchContainer = styled.div `
+const SearchContainer = styled.div`
     border: 0.5px solid lightgray;
     display: flex;
     align-items: center;
     margin-left: 25px;
     padding: 5px;
-`
+`;
+
 const Input = styled.input`
     border: none;
-    ${mobile({ width: "50px", border: "none" })}
-`
+    ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
 
 const Logo = styled.h1`
     font-weight: bold;
-    ${mobile({ fontSize: "12px" })}
-`
-
-const Center = styled.div `
-    flex: 1;
-    text-align: center;
-    ${mobile({ })}
-`
-const Right = styled.div `
+    ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
     flex: 1;
     display: flex;
     align-items: center;
     justify-content: flex-end;
-    ${mobile({ flex: 2, justifyContent: "flex-end" })}
-`
+    ${mobile({ flex: 2, justifyContent: "center" })}
+`;
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
     ${mobile({ fontSize: "12px", marginLeft: "10px" })}
-`
+`;
 
 const Navbar = () => {
     return (
-        <Container className="navbar">
+        <Sticky>
+            <Container>
             <Wrapper>
                 <Left>
                     <SearchContainer>
-                        <Input placeholder="search" style={{border: "none"}}/>
-                        <Search style={{color: "gray", fontSize:16, border: "none"}}/>
+                        <Input placeholder="Search" / >
+                        <Search style={{ color: "gray", fontSize: 16 }} />
                     </SearchContainer>
-                </Left> 
-                <Center><Logo>A Lot Of Good.</Logo></Center>
+                </Left>
+                <Center>
+                    <Logo>A Lot Of Good</Logo>
+                </Center>
                 <Right>
-                    <MenuItem>SIGN IN</MenuItem>
                     <MenuItem>
-                        <Badge badgeContent={4} color="primary">
-                            <ShoppingCartOutlinedIcon/>
-                        </Badge>
+                        <Link to={`/cart`} style={{ textDecoration: 'none' }}>
+                            <Badge badgeContent={4} color="primary">
+                                <ShoppingCartOutlinedIcon/>
+                            </Badge>
+                        </Link>
                     </MenuItem>
                 </Right>
             </Wrapper>
-            
         </Container>
-    )
-}
+        </Sticky>
+        
+    );
+};
 
-export default Navbar
+export default Navbar;
