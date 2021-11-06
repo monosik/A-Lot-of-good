ca8fbdca5c948e609cc6db72c0c1856d2f04dab7
monosik <44132642+monosik@users.noreply.github.com>
2021-11-07 00:18:40 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://7wallpapers.net/wp-content/uploads/Venom-2-7.jpg?fbclid=IwAR2HaqMB7GCHNvOUsBOx4TtV42ECGkqukGb0hEhrQgJtnc4gVXnYtaAmmzY",
+    title: "VENOM 2",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://pbs.twimg.com/media/E8-IIBbVkAEKomT.jpg",
+    title: "SHANG-CHI",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://c.wallhere.com/photos/da/77/Robert_Pattinson_The_Batman_2021_movies_artwork_Batman_red_red_background-1951245.jpg!d",
+    title: "THE BATMAN",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://www.recovery4life.co.uk/wp-content/uploads/2020/11/COVID-Rapid-Antigen-kit-scaled.jpg",
+      title: "Indoor Item",
+      cate: "Indoor",
+    },
+    {
+      id: 2,
+      img: "https://bgr.com/wp-content/uploads/2020/08/hand-sanitizer.jpg?quality=70&strip=all",
+      title: "Sanitizer",
+      cate: "Sanitizer",
+    },
+    {
+      id: 3,
+      img: "https://www.rushu.rush.edu/sites/default/files/images/news-articles/surgical-masks.jpg",
+      title: "Medical Tools",
+      cate: "Medical-tools",
+    },
+    // https://www.recovery4life.co.uk/wp-content/uploads/2020/11/COVID-Rapid-Antigen-kit-scaled.jpg
+
+    // https://bgr.com/wp-content/uploads/2020/08/hand-sanitizer.jpg?quality=70&strip=all
+
+    // https://www.rushu.rush.edu/sites/default/files/images/news-articles/surgical-masks.jpg
+  ];
+
+  // export const popularProducts = [
+  //   {
+  //     id:1,
+  //     img:"https://th-live-05.slatic.net/p/1383ea2748a593660f32339857e403eb.jpg_720x720q80.jpg_.webp",
+  //   },
+  //   {
+  //     id:2,
+  //     img:"https://th-live-05.slatic.net/p/702a12a9a67dd18ead9ab60c0048406b.png_720x720q80.jpg_.webp",
+  //   },
+  //   {
+  //     id:3,
+  //     img:"https://th-live-02.slatic.net/p/79876221b8b9bf495172a29024e27ce8.jpg",
+  //   },
+  //   {
+  //     id:4,
+  //     img:"https://th-live-05.slatic.net/p/86c3763f627859adb5a2911c47099aee.jpg_2200x2200q80.jpg_.webp",
+  //   },
+  //   {
+  //     id:5,
+  //     img:"https://cf.shopee.co.th/file/6931ea2d8b73fd469c14599efe149789",
+  //   },
+  //   {
+  //     id:6,
+  //     img:"https://th-live-02.slatic.net/p/e29853c6ee243283da070a4f9088c2fe.jpg",
+  //   },
+  //   {
+  //     id:7,
+  //     img:"https://th-live-05.slatic.net/p/4fd7ce3bbaa7eade4e195368eaffb5ab.jpg_2200x2200q80.jpg_.webp",
+  //   },
+  //   {
+  //     id:8,
+  //     img:"https://th-live-05.slatic.net/p/692196ba1bbe06231aa1d970946619d8.jpg_2200x2200q80.jpg_.webp",
+  //   },
+  // ]
